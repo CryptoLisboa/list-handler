3c5aea866d84d2028c360423359c7ce122747e34
Crypto Lisboa <dey00tsnetwork@gmail.com>
2024-05-10 19:50:53 +0100

    Subject: Refactor Leaderboard list item rendering
    Body:
    Moved the definition of renderItem function used in Leaderboard component into a separate file to improve code modularity and maintainability. This change simplifies the Leaderboard component and increases reusability of the renderItem function.
    
    No referenced issues.

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,9 +1,10 @@
 // src/components/Leaderboard.tsx
 import React from "react";
-import { View, Text, Image, FlatList, ListRenderItemInfo } from "react-native";
+import { Text, FlatList } from "react-native";
 import { useQuery } from "@tanstack/react-query";
 import { User } from "../types";
 import { fetchLeaderboard } from "../services/api";
+import { renderItem } from "./renderItem";
 
 export const Leaderboard: React.FC = () => {
     const {
@@ -21,15 +22,6 @@ export const Leaderboard: React.FC = () => {
     if (error) {
         return <Text>Error: {error.message}</Text>;
     }
-
-    const renderItem = ({ item, index }: ListRenderItemInfo<User>) => (
-        <View style={{ flexDirection: "row", padding: 10, alignItems: "center" }}>
-            <Image source={{ uri: item.picture }} style={{ width: 50, height: 50, borderRadius: 25 }} />
-            <Text style={{ marginLeft: 10 }}>
-                #{index + 1} - {item.username}
-            </Text>
-        </View>
-    );
 
     return <FlatList data={users} keyExtractor={(item) => item._id} renderItem={renderItem} />;
 };
---
+++ src/components/renderItem.tsx
@@ -0,0 +1,13 @@
+// src/components/Leaderboard2.tsx
+import React from "react";
+import { View, Text, Image, ListRenderItemInfo } from "react-native";
+import { User } from "../types";
+
+export const renderItem = ({ item, index }: ListRenderItemInfo<User>) => (
+    <View style={{ flexDirection: "row", padding: 10, alignItems: "center" }}>
+        <Image source={{ uri: item.picture }} style={{ width: 50, height: 50, borderRadius: 25 }} />
+        <Text style={{ marginLeft: 10 }}>
+            #{index + 1} - {item.username}
+        </Text>
+    </View>
+);
